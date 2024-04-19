9d221771727dfe95f66875e4a35df8c9832a1946
hc-tbhc <haz044@icloud.com>
2024-04-19 11:02:46 +0100

    6-get-comments-by-id & revisions to task 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -75,10 +75,9 @@ describe('/api/articles/:id', () => {
             .then(({ body }) => {
                 expect(body.message).toBe('404: Not found');
             })
-                expect(body.message).toEqual('Path not found');
-            })
         })
     })
+})
 
 describe('/api', () => {
     test('200: GET /api - responds with an object of the API\'s endpoints', () => {
@@ -97,10 +96,9 @@ describe('/api/articles', () => {
         .get('/api/articles')
         .expect(200)
         .then(({ body }) => {
-            const {articles} = body;
-            expect(articles.length > 0).toBe(true);
+            expect()
 
-            articles.forEach((article) =>{
+            body.forEach((article) =>{
                 expect(article).toMatchObject({
                     author: expect.any(String),
                     title: expect.any(String),
@@ -110,7 +108,6 @@ describe('/api/articles', () => {
                     votes: expect.any(Number),
                     article_img_url: expect.any(String),
                     comment_count: expect.any(String),
-                    
                 })
             })
         })
@@ -121,16 +118,52 @@ describe('/api/articles', () => {
         .get('/api/articles')
         .expect(200)
         .then(({ body }) => {
-            const {articles} = body;
-            const dates = [];
-            expect(articles.length > 0).toBe(true);
-
-            articles.forEach((article) =>{
-                dates.push(article.created_at);
-            })
-
-            expect(articles).toBeSortedBy('created_at', {
+            expect(body).toBeSortedBy('created_at', {
                 descending: true,
+            })
+        })
+    })
+})
+
+describe('/api/articles/:id/comments', () => {
+    describe('STATUS 200', () => {
+        test('200: GET /api/articles/:id/comments - responds with an array of comment objects containing the relevant keys', () => {
+            return request(app)
+            .get('/api/articles/1/comments')
+            .expect(200)
+            .then(({ body }) => {
+                body.forEach((comment) => {
+                    expect(comment).toMatchObject({
+                        comment_id: expect.any(Number),
+                        votes: expect.any(Number),
+                        created_at: expect.any(String),
+                        author: expect.any(String),
+                        body: expect.any(String),
+                        article_id: expect.any(Number)
+                    })
+                })
+            })
+        })
+
+        test('200: GET /api/articles/:id/comments - array returned should be sorted by date in descending order', () => {
+            return request(app)
+            .get('/api/articles/1/comments')
+            .expect(200)
+            .then(({ body }) => {
+                expect(body).toBeSortedBy('created_at', {
+                    descending: true,
+                })
+            })
+        })
+    })
+
+    describe('ERRORS', () => {
+        test('404: GET /api/articles/:id/comments - responds with \'404: Not found\' when passed an id that does not exist', () => {
+            return request(app)
+            .get('/api/articles/16/comments')
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.message).toBe('404: Not found');
             })
         })
     })
