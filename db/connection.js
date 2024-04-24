8a4f931d6d1be7902526323d0e9a04f6e22845e1
hc-tbhc <haz044@icloud.com>
2024-04-24 14:44:55 +0100

    updated connection file

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,12 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 if (ENV === 'production') {
@@ -15,4 +19,4 @@ if (ENV === 'production') {
   config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
